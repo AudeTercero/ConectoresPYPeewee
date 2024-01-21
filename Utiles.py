0a5a5c10e64a9a0ef1ea74add8c9ede7148dc399
AudeTercero <92akumanomi@gmail.com>
2024-01-21 20:27:15 +0100

    Utiles y coloreado alumnos

---
+++ Utiles.py
@@ -0,0 +1,26 @@
+from colorama import Fore, Style
+
+
+def rojo(msg):
+    print(Fore.RED + msg + Fore.RESET)
+
+
+def amarillo(msg):
+    print(Fore.LIGHTYELLOW_EX + msg + Fore.RESET)
+
+
+def azul(msg):
+    print(Style.BRIGHT)
+    print(Fore.CYAN + msg + Fore.RESET)
+    print(Style.RESET_ALL)
+
+
+def verde(msg):
+    print(Fore.GREEN + msg + Fore.RESET)
+
+
+def siNone(str):
+    if str is 'None':
+        return 'Vacio'
+    else:
+        return str
